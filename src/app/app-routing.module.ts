16fd32a5b3f557c2659e15f1ff379bbe0eb9b90f
Salsrefil <240789@edu.p.lodz.pl>
2022-05-28 15:52:14 +0200

    Dodano routing (przeładowanie komponentów), domyślny komponent + 2 zakładki

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { FormularzeComponent } from './formularze/formularze.component';
+import { HomeComponent } from './home/home.component';
+import { OfferComponent } from './offer/offer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'signup', component: FormularzeComponent},
+  {path: 'offer', component: OfferComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'contact', component: ContactComponent},
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, FormularzeComponent, OfferComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormularzeComponent } from './formularze/formularze.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { OfferComponent } from './offer/offer.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component'
 
 
 @NgModule({
 declarations: [
 AppComponent,
 FormularzeComponent,
-HomeComponent
+HomeComponent,
+routingComponents,
+OfferComponent,
+FaqComponent,
+ContactComponent
 ],
 imports: [
 BrowserModule,
