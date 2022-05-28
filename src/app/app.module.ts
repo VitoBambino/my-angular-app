d4ddba0beaf33a29b0995f21eb12ad5659bec344
Salsrefil <240789@edu.p.lodz.pl>
2022-05-28 14:23:07 +0200

    nowy app-component - na bootstrapie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormularzeComponent } from './formularze/formularze.component'
+import { FormularzeComponent } from './formularze/formularze.component';
+import { HomeComponent } from './home/home.component'
 
 
 @NgModule({
 declarations: [
 AppComponent,
-FormularzeComponent
+FormularzeComponent,
+HomeComponent
 ],
 imports: [
 BrowserModule,
