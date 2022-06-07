1e05efb855bdeb0b5563c5b01014efd5d49b5634
Salsrefil <240789@edu.p.lodz.pl>
2022-06-07 13:35:15 +0200

    Backend

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,7 +13,22 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit(value: any){
-    console.log(value);
+    var form=JSON.stringify(value);
+    let req = new XMLHttpRequest();
+    
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        console.log(req.responseText);
+      }
+    };
+      
+    req.open("POST", "https://api.jsonbin.io/v3/b", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("X-Master-Key", "$2b$10$TycnQVgsRvh3d7uhhd4D/uJeEDuXlsW/nWsjQ34ZR3QJr9H4gFyp.");
+    req.setRequestHeader("X-Collection-Id", "629f372c449a1f3821006106");
+    req.setRequestHeader("X-Bin-Private", "false");
+    req.send(form);
+    
   }
 
 }
--- src/app/formularze/formularze.component.ts
+++ src/app/formularze/formularze.component.ts
@@ -17,10 +17,23 @@ export class FormularzeComponent implements OnInit {
   }
 
   onSubmit(value: any){
-    console.log(value);
+    var form=JSON.stringify(value);
+let req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+  if (req.readyState == XMLHttpRequest.DONE) {
+    console.log(req.responseText);
   }
+};
+  
+req.open("POST", "https://api.jsonbin.io/v3/b", true);
+req.setRequestHeader("Content-Type", "application/json");
+req.setRequestHeader("X-Master-Key", "$2b$10$TycnQVgsRvh3d7uhhd4D/uJeEDuXlsW/nWsjQ34ZR3QJr9H4gFyp.");
+req.setRequestHeader("X-Collection-Id", "629de210449a1f3821ff5d86");
+req.setRequestHeader("X-Bin-Private", "false");
+req.send(form);
 
-
+}
 
 }
 
